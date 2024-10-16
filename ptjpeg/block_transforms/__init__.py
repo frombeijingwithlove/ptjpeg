8110bf34a18bcf8fd2f31f23e32b09451e6734dc
yey <7559310+yeyun111@users.noreply.github.com>
2024-10-16 11:11:28 +0800

    Update __init__.py

--- ptjpeg/block_transforms/__init__.py
+++ ptjpeg/block_transforms/__init__.py
@@ -1,7 +1,9 @@
 from .base import BlockTransform, InverseBlockTransform
 from .dct import DCT, IDCT
+from .dense_dct import DenseDCT, DenseIDCT
 
 __all__ = [
     'BlockTransform', 'InverseBlockTransform',
-    'DCT', 'IDCT'
+    'DCT', 'IDCT', 
+    'DenseDCT', 'DenseIDCT'
 ]
