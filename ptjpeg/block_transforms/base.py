4c1197838c67eba4eefa88adc0a36b37dd4a94d2
yey <7559310+yeyun111@users.noreply.github.com>
2024-10-16 11:04:38 +0800

    Update base.py with dense op

--- ptjpeg/block_transforms/base.py
+++ ptjpeg/block_transforms/base.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from torch import Tensor
 
 
-class _BlockTransformKernels(nn.Module):
+class _KernelTransform(nn.Module):
 
     def __init__(self, kernels: Tensor):
         super().__init__()
@@ -10,8 +10,8 @@ class _BlockTransformKernels(nn.Module):
         self.register_buffer('kernels', kernels.unsqueeze(1))
 
 
-class BlockTransform(_BlockTransformKernels):
-    __doc__ = r"""Block Discrete Transform (DCT) implemented in torch convolution
+class BlockTransform(_KernelTransform):
+    __doc__ = r"""Block Transform implemented in torch convolution
     """
 
     def forward(self, im: Tensor) -> Tensor:
@@ -20,7 +20,8 @@ class BlockTransform(_BlockTransformKernels):
             im.reshape(n * c, 1, h, w),
             self.kernels,
             bias=None,
-            stride=(self.kh, self.kw),
+            stride=(self.kh, self.kw), 
+            0, 1, 1
         )
         return res.view(
             n,
@@ -31,8 +32,8 @@ class BlockTransform(_BlockTransformKernels):
         )
 
 
-class InverseBlockTransform(_BlockTransformKernels):
-    __doc__ = r"""Block Discrete Transform implemented in torch convolution
+class InverseBlockTransform(_KernelTransform):
+    __doc__ = r"""Inverse Block Transform implemented in torch convolution
         """
 
     def forward(self, subbands: Tensor) -> Tensor:
@@ -41,7 +42,8 @@ class InverseBlockTransform(_BlockTransformKernels):
             subbands.reshape(-1, d, hs, ws),
             self.kernels,
             bias=None,
-            stride=(self.kh, self.kw),
+            stride=(self.kh, self.kw), 
+            0, 0, 1, 1
         )
         return im_restored.view(
             n,
@@ -49,3 +51,43 @@ class InverseBlockTransform(_BlockTransformKernels):
             hs * self.kh,
             ws * self.kw
         )
+
+
+class DenseTransform(_KernelTransform):
+    __doc__ = r"""Dense Transform implemented in torch convolution
+    """
+
+    def forward(self, im: Tensor) -> Tensor:
+        n, c, h, w = im.size()
+        res = nn.functional.conv2d(
+            im.reshape(n * c, 1, h, w),
+            self.kernels,
+            bias=None,
+            stride=1, 
+            0, 1, 1
+        )
+        return res.view(
+            n,
+            c,
+            self.num_subbands,
+            h // self.kh,
+            w // self.kw, 
+        )
+
+
+class InverseBlockTransform(_KernelTransform):
+    __doc__ = r"""Inverse Dense Transform implemented in torch convolution
+        """
+
+    def forward(self, subbands: Tensor) -> Tensor:
+        n, c, d, hs, ws = subbands.size()
+        im_restored = nn.functional.conv_transpose2d(
+            subbands.reshape(-1, d, hs, ws),
+            self.kernels,
+            bias=None,
+            stride=1, 
+            0, 0, 1, 1
+        )
+        im_restored = im_restored[:, :, self.kh-1:1-self.kh, self.kw-1:1-self.kw]
+        *_, h, w = im_restored.size()
+        return im_restored.view(n, c, h, w) / self.num_subbands
